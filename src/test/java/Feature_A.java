f12b47381231ab4db8c76e3e1b5d703e54902148
Jagodinac91 <74147076+Jagodinac91@users.noreply.github.com>
2021-01-26 11:23:17 -0800

    New comment on the new branch

--- src/test/java/Feature_A.java
+++ src/test/java/Feature_A.java
@@ -1,5 +1,6 @@
 public class Feature_A {
     public static void main(String[] args) {
         System.out.println("Feature A started");
+        System.out.println("B21 some comment added");
     }
 }
