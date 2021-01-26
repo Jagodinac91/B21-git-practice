bc71cdb0d0a993a201342c50050779d22d9fed1c
Jagodinac91 <74147076+Jagodinac91@users.noreply.github.com>
2021-01-26 12:25:32 -0800

    movie 1 branch update hello class again

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, B21");
         System.out.println("It's such a lovely day");
-        //watching movies
+        System.out.println("More action");
+        System.out.println("Ready to go back");
     }
 }
